0b239cf4819fb9947e8afaf73656e423ab9ff763
Tim Fox <tim.fox@uk.tesco.com>
2017-06-30 11:16:10 +0100

    Upgraded to vert.x 3.4.2 and removed work around for AsyncFile bug

--- src/main/java/io/mewbase/server/impl/file/af/AFBasicFile.java
+++ src/main/java/io/mewbase/server/impl/file/af/AFBasicFile.java
@@ -2,15 +2,11 @@ package io.mewbase.server.impl.file.af;
 
 import io.mewbase.server.impl.BasicFile;
 import io.mewbase.util.AsyncResCF;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.file.AsyncFile;
-import io.vertx.core.file.impl.AsyncFileImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Method;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -52,37 +48,11 @@ public class AFBasicFile implements BasicFile {
         return cf;
     }
 
-    /*
-    Workaround for https://github.com/eclipse/vert.x/issues/2012
-    Can be removed once it's fixed
-    We're going to call the private doClose method directly to force a close of the file and prevent a hang
-     */
-    private static Method AFI_DOCLOSE_METHOD;
-    static {
-        try {
-            AFI_DOCLOSE_METHOD = AsyncFileImpl.class.getDeclaredMethod("doClose", Handler.class);
-            AFI_DOCLOSE_METHOD.setAccessible(true);
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
-    }
-
     public CompletableFuture<Void> close() {
         AsyncResCF<Void> ar = new AsyncResCF<>();
         af.flush(res -> {
             if (res.succeeded()) {
-                try {
-                    Handler<AsyncResult<Void>> handler = res2 -> {
-                        if (res2.succeeded()) {
-                            ar.complete(res2.result());
-                        } else {
-                            ar.completeExceptionally(res2.cause());
-                        }
-                    };
-                    AFI_DOCLOSE_METHOD.invoke(af, handler);
-                } catch (Exception e) {
-                    ar.completeExceptionally(e);
-                }
+                af.close(ar);
             } else {
                 ar.completeExceptionally(res.cause());
             }
