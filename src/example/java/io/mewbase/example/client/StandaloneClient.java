d523b5911c3cb69f45326525ac075ddd328a734f
NigeWarren <Nige@zink-digital.com>
2017-07-31 15:03:29 +0100

    Improve StandaloneClient

--- src/example/java/io/mewbase/example/client/StandaloneClient.java
+++ src/example/java/io/mewbase/example/client/StandaloneClient.java
@@ -8,34 +8,40 @@ import io.mewbase.client.Producer;
 import java.util.concurrent.CompletableFuture;
 
 /**
- * This is an example of a client that can be used with a running server.
+ * This is an example of a Client that can be used with a Mewbase server running
+ * in a separate JVM; either on the same machine or over the network.
  *
- * To get a server running on your local machine see the README.MD
+ * To get a server running on your local or remote machine see the project
+ * level README.MD
  *
  * Once a server is running - we assume on localhost and on the default port -
  * this client will then write a set of events to the server.
  *
+ * Look to the HOST and PORT values if you run remotely and/or on another port.
+ *
+ * Change the EVENT_COUNT to the number of events that you want to
  */
 public class StandaloneClient {
 
-    private static final int EVENTS = 1000;
+
+    private static final String HOST = "localhost"; // also as  ClientOptions.DEFAULT_HOST;
+    private static final int PORT = 7451; // also as ClientOptions.DEFAULT_PORT;
+
+    private static final int EVENT_COUNT = 1000;
+
+    private static final String CHANNEL = "Channel1";
+
 
 
     public static void main(String[] args) {
         try {
-
             new StandaloneClient().run();
-            System.out.println(EVENTS + " events published to server.");
+            System.out.println(EVENT_COUNT + " events published to server.");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
-    private static final String HOST = "localhost"; // ClientOptions.DEFAULT_HOST;
-    private static final int PORT = 7451; // ClientOptions.DEFAULT_PORT;
-
-    private static final String CHANNEL = "Channel1";
 
     public void run() throws Exception {
         ClientOptions opts = new ClientOptions().
@@ -52,8 +58,9 @@ public class StandaloneClient {
         Producer producer = client.createProducer(CHANNEL);
 
         // write EVENTS number of events to the Channel.
-        for (int  i = 0;  i < EVENTS ; ++i) {
-            final CompletableFuture<Void> published = producer.publish(new BsonObject().put("foo", "bar").put("num", i));
+        for (int  i = 0;  i < EVENT_COUNT ; ++i) {
+            final BsonObject event = new BsonObject().put("foo", "bar").put("num", i);
+            final CompletableFuture<Void> published = producer.publish(event);
             Thread.sleep(10);
             published.whenComplete( (good,bad) -> {
                if (bad != null) {
